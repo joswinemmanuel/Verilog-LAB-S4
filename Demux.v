561fe24c32f204c9ce022c3f152ce53453a6771e
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2023-06-23 15:19:27 +0530

    1:4 Demux using Verilog

---
+++ Demux.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:12:45 06/23/2023 
+// Design Name: 
+// Module Name:    Demux 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Demux(
+    output a,
+    output b,
+    output c,
+    output d,
+    input din,
+    input x,
+    input y
+    );
+		assign a = din & (~x) & (~y);
+		assign b = din & (~x) & y;
+		assign c = din & x & (~y);
+		assign d = din & x & y;
+
+
+endmodule
