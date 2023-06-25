be0132f5567d85fdfbd93a55e8193fa4a7705eba
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2023-06-25 16:01:34 +0530

    JK Flip Flop

--- Flip-Flops/jkff.v
+++ Flip-Flops/jkff.v
@@ -18,21 +18,24 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module jkff(
-    input j,k,clk,
-    output reg q,qb
-    );
-initial 
-q=1'b0;
 
-always @(posedge clk)
-begin
-case({j,k})
-2'b00:q=q;
-2'b01:q=1'b0;
-2'b10:q=1'b1;
-2'b11:q=(~q);
-endcase
-qb=(~q);
-end
+module jkff(
+    input j, k, clk,
+    output reg q, qb
+    );
+    
+    initial 
+    q=1'b0;
+
+    always @(posedge clk)
+    begin
+        case({j,k})
+            2'b00:q=q;
+            2'b01:q=1'b0;
+            2'b10:q=1'b1;
+            2'b11:q=(~q);
+        endcase
+    qb=(~q);
+    end
+    
 endmodule
