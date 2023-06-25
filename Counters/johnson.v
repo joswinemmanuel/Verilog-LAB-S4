1142badb3b87c44f8fcc26565eeeaa7ac03f08bf
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2023-06-25 16:09:48 +0530

    Johnson counter

--- Counters/johnson.v
+++ Counters/johnson.v
@@ -18,22 +18,24 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
+
 module johnson(
     input clk,
     output reg[0:3]q=4'b0000
     );
 
-always @(posedge clk)
-begin
-case(q)
-4'b0000:q=4'b1000;
-4'b1000:q=4'b1100;
-4'b1100:q=4'b1110;
-4'b1110:q=4'b1111;
-4'b1111:q=4'b0111;
-4'b0111:q=4'b0011;
-4'b0011:q=4'b0001;
-4'b0001:q=4'b0000;
-endcase
-end
+    always @(posedge clk)
+    begin
+        case(q)
+            4'b0000:q=4'b1000;
+            4'b1000:q=4'b1100;
+            4'b1100:q=4'b1110;
+            4'b1110:q=4'b1111;
+            4'b1111:q=4'b0111;
+            4'b0111:q=4'b0011;
+            4'b0011:q=4'b0001;
+            4'b0001:q=4'b0000;
+        endcase
+    end
+    
 endmodule
