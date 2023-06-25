af5f013b8a1447ea9295b013f7307f3f82318db3
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2023-06-25 15:59:55 +0530

    SR Flip Flop

--- Flip-Flops/srff.v
+++ Flip-Flops/srff.v
@@ -18,22 +18,23 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module srff(
-    input s,r,clk,
-    output reg q,qb
-    );
-initial
-q=1'b0;
 
-always @(posedge clk)
-begin
-case({s,r})
-2'b00:q=q;
-2'b01:q=1'b0;
-2'b10:q=1'b1;
-2'b11:q=1'bz;
-endcase
-qb=~q;
-end 
+module srff(
+    input s, r, clk,
+    output reg q, qb
+    );
+    initial
+    q=1'b0;
+
+    always @(posedge clk)
+    begin
+        case({s,r})
+            2'b00:q=q;
+            2'b01:q=1'b0;
+            2'b10:q=1'b1;
+            2'b11:q=1'bz;
+        endcase
+    qb=~q;
+    end 
 
 endmodule
